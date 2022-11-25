58198f9a0d35f5cdff7ad119ffeeb4e96ff8a766
Pc <Pc@192.168.1.61>
2022-11-25 18:07:44 -0500

    qwq

--- src/main/java/Modelo/PlanCurricular.java
+++ src/main/java/Modelo/PlanCurricular.java
@@ -1,14 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Modelo;
 
 import java.util.Vector;
-/**
- *
- * @author Usuario
- */
+
 public class PlanCurricular {
     private Vector<Curso> cursos;
     private String anio;
